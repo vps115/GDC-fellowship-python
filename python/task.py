f7e6a6976f973e58a48a20d9ea45db0f0972b896
Vidhi <shah.vidhi115@gmail.com>
2021-12-13 17:53:50 +0530

    Report completed

--- python/task.py
+++ python/task.py
@@ -8,47 +8,50 @@ $ ./task done INDEX           # Mark the incomplete item with the given index as
 $ ./task help                 # Show usage
 $ ./task report               # Statistics
 '''
-def read():
-    task.seek(0)
-    l=task.readlines()
-    l.sort(key=lambda x:x[0])
-    return l
-
-task = open("task.txt","a+")
-completed = open("completed.txt","a+")
 
 if len(sys.argv)==1 or sys.argv[1]=='help':
     sys.stdout.buffer.write(help.encode('utf8'))
 
 elif sys.argv[1]=='add':
     if len(sys.argv)>2:
+        with open("task.txt", "a+") as f:
+            f.write(' '.join(sys.argv[2:]))
+            f.write('\n')
+            f.seek(0)
+            t=f.readlines()
+            t.sort(key=lambda x:x[0])
+        with open("task.txt", "w") as f:
+                for i in t:
+                    f.write(i)
         msg='Added task: "{}" with priority {}'.format(' '.join(sys.argv[3:]),sys.argv[2])
-        task.write(' '.join(sys.argv[2:]))
-        task.write('\n')
         sys.stdout.buffer.write(msg.encode('utf8'))
     else:
         msg='Error: Missing tasks string. Nothing added!'
         sys.stdout.buffer.write(msg.encode('utf8'))
 
 elif sys.argv[1]=='ls':
-    t=read()
-    if len(t)>0:
-        for i in range(len(t)):
-            msg='{}. {} [{}]\n'.format(i+1,t[i][2:-1],t[i][0])
+    with open("task.txt", "a+") as f:
+        f.seek(0)
+        t=f.readlines()
+        if len(t)>0:
+            for i in range(len(t)):
+                msg='{}. {} [{}]\n'.format(i+1,t[i][2:-1],t[i][0])
+                sys.stdout.buffer.write(msg.encode('utf8'))
+        else:
+            msg = 'There are no pending tasks!'
             sys.stdout.buffer.write(msg.encode('utf8'))
-    else:
-        msg = 'There are no pending tasks!'
-        sys.stdout.buffer.write(msg.encode('utf8'))
 
 elif sys.argv[1]=='del':
     if len(sys.argv)>2:
         index=sys.argv[2]
-        t=read()
+        with open("task.txt", "a+") as f:
+            f.seek(0)
+            t=f.readlines()
         if int(index)>len(t) or int(index)<1:
             msg='Error: task with index #{} does not exist. Nothing deleted.'.format(index)
             sys.stdout.buffer.write(msg.encode('utf8'))
         else:
-            with open("tasks.txt", "w") as f:
+            with open("task.txt", "w") as f:
                 for i in range(len(t)):
                     if i+1 != int(index):
                         f.write(t[i])
@@ -61,22 +64,41 @@ elif sys.argv[1]=='del':
 elif sys.argv[1]=='done':
     if len(sys.argv)>2:
         index=sys.argv[2]
-        t=read()
+        with open("task.txt", "a+") as f:
+            f.seek(0)
+            t=f.readlines()
         if int(index)>len(t) or int(index)<1:
             msg='Error: no incomplete item with index #{} exists.'.format(index)
             sys.stdout.buffer.write(msg.encode('utf8'))
         else:
             d=t.pop(int(index)-1)
-            with open("tasks.txt", "w") as f:
+            with open("task.txt", "w") as f:
                 for i in t:
                         f.write(i)
-            completed.write(d)
+            with open("completed.txt", "a") as f:
+                f.write(d[2:])
             msg='Marked item as done.'
             sys.stdout.buffer.write(msg.encode('utf8'))
     else:
         msg = 'Error: Missing NUMBER for marking tasks as done.'
         sys.stdout.buffer.write(msg.encode('utf8'))
 
-
-task.close()
-completed.close()
+elif sys.argv[1]=='report':
+    with open("task.txt", "a+") as f:
+            f.seek(0)
+            t=f.readlines()
+    msg='Pending : {}\n'.format(len(t))
+    sys.stdout.buffer.write(msg.encode('utf8'))
+    if len(t)>0:
+        for i in range(len(t)):
+            msg='{}. {} [{}]'.format(i+1,t[i][2:-1],t[i][0])
+            sys.stdout.buffer.write(msg.encode('utf8'))
+    with open("completed.txt", "a+") as f:
+            f.seek(0)
+            t1=f.readlines()
+    msg='\n\nCompleted : {}\n'.format(len(t1))
+    sys.stdout.buffer.write(msg.encode('utf8'))
+    if len(t1)>0:
+        for i in range(len(t1)):
+            msg='{}. {}'.format(i+1,t1[i])
+            sys.stdout.buffer.write(msg.encode('utf8'))
